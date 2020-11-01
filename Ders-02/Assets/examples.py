8267744d6f70d3754dd27f239a730e3e3d7402b0
Burak Büyükyüksel <buyukyukselburak@gmail.com>
2020-11-01 15:27:54 +0300

    Ders-02

---
+++ Ders-02/Assets/examples.py
@@ -0,0 +1,69 @@
+#C:Büyük Yorum Satırı
+print("Merhaba Dünya") 
+print('Merhaba Dünya')
+print("Merhaba Dünya'")
+print('Merhaba "Kerem" programına hoşgeldin!')
+
+#C:Burası yorum satırıdır.
+#C:Kerem aşağıdaki örnek hatalı bir kullanımdır.
+#print("Merhaba "Kerem" programına hoşgeldin!")
+#print('Merhaba 'Kerem' programına hoşgeldin!')
+
+# -------------------------------------------
+print("3+4=", 3+4)
+print("MerhabaKeremBugünNasılsın?")
+print("Merhaba", "Kerem", "Bugün", "Nasılsın", "?")
+# -------------------------------------------
+print("2 tane sayıyı topluyorum", 3+4)
+# -------------------------------------------
+''' Değişken Tanımlama '''
+sayi1 = 5
+sayi2 = 10
+
+print("sayi1 + sayi2 =", sayi1+sayi2)
+print(sayi1, "+", sayi2, "=", sayi1+sayi2)
+print(5, "+", 10, "=", 5+10)
+print("5", 5)
+
+#1. Aşama
+print("1.Aşama:", sayi1, "+", sayi2, "=", sayi1+sayi2)
+#2. Aşama
+print("2.Aşama:", 5, "+", 10, "=", 5+10)
+#3. Aşama
+print("3.Aşama:", "5", "+", "10", "=", "5+10")
+
+# -----------------------------------------------
+# Ekrana ne yazar?
+print(5)
+print(10.2)
+print("1234")
+
+'''
+# Tipler
+integer : Tam sayı       (Noktasız Sayı) örnek: 5, 3, 2, 10, 99, -123, -12
+float   : Küsüratlı Sayı (Noktalı  Sayı) örnek: 99.9, 12.5, 3.5, 3.14, 5.5, 5.75, 2.5, 2.00000001
+string  : Kelimeler, harfler, cümleler   örnek: 'lego', "lego", "bu bir cümledir.", 'bu bir cümledir.'
+'''
+x = 5965.3
+print("x değişkenin değeri:", x)
+print("x değişkeninin tipi:", type(x))
+
+# Dönüşüm Fonksiyonlarımız
+#int(sayi)
+#float(sayi)
+#str(sayi)
+
+sayi = float(365)
+print("Sayi'nin tipi", type(sayi))
+
+
+
+
+
+
+
+
+
+
+
+
