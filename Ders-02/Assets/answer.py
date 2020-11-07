3612486212eb2c80f6a5c5c9cb0918a11270a7c9
Burak Büyükyüksel <buyukyukselburak@gmail.com>
2020-11-07 11:44:46 +0300

    Ders2'nin soru 1 ve soru 2'si çözüldü

---
+++ Ders-02/Assets/answer.py
@@ -0,0 +1,23 @@
+# Soru 1)
+kerem_odev_01 = str(1453)
+kerem_odev_01 = int(kerem_odev_01)
+# ------------------------------------- <Yorum İşareti, Comment>
+kerem_odev_01 = 1453
+kerem_odev_01 = str(kerem_odev_01) 
+kerem_odev_01 = int(kerem_odev_01) 
+kerem_odev_01 = kerem_odev_01 + 1
+print('kerem_odev_01:', kerem_odev_01, type(kerem_odev_01))
+
+# Soru 2)
+kerem_odev_02 = 101
+kerem_odev_02 = str(kerem_odev_02)
+kerem_odev_02 = float(kerem_odev_02)
+kerem_odev_02 = kerem_odev_02 + 0.911
+print('kerem_odev_02:', kerem_odev_02, type(kerem_odev_02))
+
+# Soru 3)
+
+
+
+
+
